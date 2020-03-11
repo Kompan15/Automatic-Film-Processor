9edc75e84e34875e318aea373bb4171c94041cd1
Kompan15 <rafal.mistela@gmail.com>
2020-03-11 13:13:29 +0100

    Improved time displaying: Minutes:Seconds, added more comments

--- Processor/Processor.ino
+++ Processor/Processor.ino
@@ -2,7 +2,7 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 using namespace std;
-//stale obslugi sterownika silnika
+//stałe obslugi sterownika silnika
 const int Enable_B = 3;
 const int Wejscie_Sterownika_3 = 5;
 const int Wejscie_Sterownika_4 = 4;
@@ -10,9 +10,9 @@ const int Wejscie_Sterownika_4 = 4;
 const int Przycisk_1 = 8;
 const int Przycisk_2 = 9;
 const int Przycisk_3 = 10;
-signed int stanSwitch = 0; //signed, bo nie przyjmuje wartości ujemnych
+unsigned int stanSwitch = 0; //unsigned, bo nie przyjmuje wartości ujemnych
 //zmienna czasowa.
-signed int czas;
+unsigned int czas;
 //Deklaracje funkcji, inaczej tablica wskaznikow nie zadziala, kompilator musi wiedziec wczesniej.
 void Wywolywanie();
 void Przerywanie();
@@ -20,49 +20,55 @@ void Utrwalanie();
 void Plukanie();
 void Uzytkownika();
 void Predefined_D74();
-//deklaracja tablicy programów
-String programy[] = {"Wywolywanie", "Przerywanie", "Utrwalanie", "Plukanie", "Uzytkownika", "Predefined D-74"}; //tablica Stringów, słowo to jeden obiekt
+/*deklaracja tablicy programów - jeden string to jeden obiekt, planowałem zrobić to na wektorach, ale nie są wskazane
+z tak małą ilością pamięci wewnętrznej*/
+String programy[] = {"Wywolywanie", "Przerywanie", "Utrwalanie", "Plukanie", "Uzytkownika", "Predefined D-74"}; 
 //definicja tablicy wskaznikow do programow.
 void (*programy_wskazniki[])(void) = {Wywolywanie, Przerywanie, Utrwalanie, Plukanie, Uzytkownika, Predefined_D74};
+/*ważna informacja: chcesz dodać nową funkcję, dodajesz ją na tym samym indeksie w obydwóch powyzszych tablicach!*/
 void setup()
 {
-  /*Deklaracja przycisków*/
+  /*Deklaracja pinów przycisków w charakterze INPUT_PULLUP*/
   pinMode(Przycisk_1, INPUT_PULLUP);
   pinMode(Przycisk_2, INPUT_PULLUP);
   pinMode(Przycisk_3, INPUT_PULLUP);
-  //Ustawienie charakteru Pinow.
+ //deklaracja pinów kolejno: EnableB (PWM), i odpowiadających za kierunek krecenia.
   pinMode(Enable_B, OUTPUT);
   pinMode(Wejscie_Sterownika_3, OUTPUT);
   pinMode(Wejscie_Sterownika_4, OUTPUT);
-  //wylacz silnik.
+  //upewnij się że silnik nie działa.
   digitalWrite(Wejscie_Sterownika_3, LOW);
   digitalWrite(Wejscie_Sterownika_4, LOW);
   lcd.begin(16,2); //defincja 16 segmentów w dwóch rzędach.
-  Serial.begin(9600); //rozpocznij komunikację na porcie szeregowym
+  Serial.begin(9600); //rozpocznij komunikację na porcie szeregowym (9600Hz)
 }
-void loop() //pętla główna.
+void loop() /***********************************************************************************************pętla główna*/
 {
-  Menu();
-  Init(); //init wywołuje funkcje
+  Menu(); //menu generuje ekran powitalny wraz z menu 'karuzelowym'.
+  Init(); 
 }
+/**********************************************************************************************************koniec pętli głównej*/
+/**********************************************************************************************************Menu główne*/
 void Menu(){
-  int StanPrzycisku_1 = 0;
-  int StanPrzycisku_2 = 0;
-  int StanPrzycisku_3 = 0;
+  int StanPrzycisku_1 = 1;
+  int StanPrzycisku_2 = 1;
+  int StanPrzycisku_3 = 1;
   lcd.clear();
   lcd.setCursor(4,0);
   lcd.print("Wybierz");
   lcd.setCursor(4,1);
   lcd.print("Program");
   
-  delay(1800); //zapobiega wywołaniu programu.
+  delay(1800); //zapobiega wywołaniu programu (ms)
   do{
+    //czytaj stany przycisków a następnie przypisz je do zmiennych.
     StanPrzycisku_1 = digitalRead(Przycisk_1);
     StanPrzycisku_2 = digitalRead(Przycisk_2);
     StanPrzycisku_3 = digitalRead(Przycisk_3);
     
-    if (StanPrzycisku_1) {stanSwitch--;}
-    if (StanPrzycisku_3) {stanSwitch++;}
+    //zwiększaj/zmniejszaj indeks tablic.
+    if (!StanPrzycisku_1) {stanSwitch--;}
+    if (!StanPrzycisku_3) {stanSwitch++;}
     //warunek kręcący.
     if (stanSwitch == 6){stanSwitch = 0;}
     if (stanSwitch == -1){stanSwitch = 5;}
@@ -74,16 +80,18 @@ void Menu(){
     lcd.print("<<     OK     >>"); delay(140);
   }while(StanPrzycisku_2); //dopóki stan przycisku jest HIGH
 }
+/**********************************************************************************************************koniec menu głównego*/
+/**********************************************************************************************************funkcja inicjalizująca*/
 void Init(){
-  int StanPrzycisku_2 = 0; //inizjalizacja wartości zmiennej przechowującej stan przycisku na False.
+  int StanPrzycisku_2 = 0; //inicjalizacja wartości zmiennej przechowującej stan przycisku na False(0)
   bool znacznik_przerwania = 0; 
-  unsigned long time1;
+  unsigned long czas_na_przerwanie;
   String Stme; //sekundy w stringu.
   lcd.clear();
-  time1 = millis();
-  while((millis()-time1)<2000){ //pętla Menu głównego.
+  czas_na_przerwanie = millis();
+  while((millis()-czas_na_przerwanie)<2000){ //pętla Menu głównego.
     StanPrzycisku_2 = digitalRead(Przycisk_2);
-    Stme = String((millis() - time1)/1000);
+    Stme = String((millis() - czas_na_przerwanie)/1000);
     lcd.setCursor(0,0);
     lcd.print(programy[stanSwitch] + " za:" + Stme);
     lcd.setCursor(0,1); //drugi wiersz, pierwsza kolumna.
@@ -95,27 +103,25 @@ void Init(){
   } /*czytaj: jezeli nie przerwano - kontynuuj*/
    if(!znacznik_przerwania) programy_wskazniki[stanSwitch](); //argument funkcji programy to string z nazwa wybranego programu.
 }
+/**********************************************************************************************************koniec inicjalizatora*/
+/**********************************************************************************************************Program wykonawczy*/
 //następująca funkcja posiada dwa argumenty domniemane.
-void Program(String funkcja, unsigned long BreakTime = 0, unsigned long WTime = 0){ //string zostaje przejety tylko i wylacznie po to by wyswietlic dobry komunikat na ekran.
-  /****************************************************************/
-  /*zerujemy stany*/
-  
-  int StanPrzycisku_1 = 0;
-  int StanPrzycisku_2 = 0;
-  int StanPrzycisku_3 = 0;
+//string zostaje przejety tylko i wylacznie po to by wyswietlic dobry komunikat na ekran.
+void Program(String funkcja, unsigned long BreakTime = 0, unsigned long WTime = 0){ 
+//jedynki, PULLUP!  
+  int StanPrzycisku_1 = 1;
+  int StanPrzycisku_2 = 1;
+  int StanPrzycisku_3 = 1;
   String SPWM, Stme, StmeB; //PWM,Sekundy w string.
   unsigned int PWM = 40;
-  unsigned long time1, time2; //lokalny czas funkcji wywolanie.
+  unsigned long czas_ref_trwania_programu, czas_ref_trwania_kierunku_obrotu, minuty = 0; //zmienne czasowe.
   bool K1 = 0,K2 = 1; //inicjalnie: false
   if (!BreakTime && !WTime){ //warunek sprawdzający czy argumenty zostały domniemane czy nie.
-  /****************************************************************/
-  Serial.println("BreakTime and Wtime true");
   unsigned int znacznik_opcji = 0; //liczymy do 2 (3 - 0,1,2)
-  // bool znacznik_przerwania = 0; //w tym podprogramie nie można przerywać - ograniczenie sprzętowe.
-  bool znacznik_zatwierdzenia =0;
+  bool znacznik_zatwierdzenia = 0;
   lcd.clear(); //czysc ekran przed wejsciem do petli, inaczej wystapia artefakty.
   while(znacznik_opcji != 2){ //pętla przyjmujaca czas wykonywania wywolywania
-    //przyciski, bez nich nie wyjdziesz z petli, nie zmienisz czasu.
+    //przyciski, bez nich nie wyjdziesz z petli.
     StanPrzycisku_1 = digitalRead(Przycisk_1);
     StanPrzycisku_2 = digitalRead(Przycisk_2);
     StanPrzycisku_3 = digitalRead(Przycisk_3);
@@ -142,7 +148,7 @@ void Program(String funkcja, unsigned long BreakTime = 0, unsigned long WTime = 
       lcd.print("Czas trw.prg:");
       lcd.setCursor(0,1);
       lcd.print("<- OK +> "+ Stme + "[min]"); delay(140);
-      if (!StanPrzycisku_2){delay(140); znacznik_opcji = 1;delay(140);}break;
+      if (!StanPrzycisku_2){delay(140); znacznik_opcji = 1; delay(140);}break;
       /******************************************************************************************************/
     case 1:
       if (!StanPrzycisku_1 && BreakTime <20000) { //jeżeli obydwie wartości są True.
@@ -188,9 +194,9 @@ void Program(String funkcja, unsigned long BreakTime = 0, unsigned long WTime = 
   /********************************************************************/
   
   //odebranie czasu do zmiennej, funkcja zwraca naliczone milisekundy od momentu uruchomienia ukladu
-  time1 = millis();//czas wykorzystywany do obliczania czasu trwania programu.
-  time2 = millis();//czas wykorzystywany do obliczania czasu trwania obrotu
-  while(((millis()-time1) < WTime*60)){ //pętla WYKONANIA FUNKCJI WYWOLANIE przez okreslony czas
+  czas_ref_trwania_programu = millis();//czas wykorzystywany do obliczania czasu trwania programu.
+  czas_ref_trwania_kierunku_obrotu = millis();//czas wykorzystywany do obliczania czasu trwania obrotu
+  while(((millis()-czas_ref_trwania_programu) < WTime*60)){ //pętla WYKONANIA FUNKCJI przez okreslony czas
     
     
     //czytaj stany przycisków
@@ -198,11 +204,19 @@ void Program(String funkcja, unsigned long BreakTime = 0, unsigned long WTime = 
     StanPrzycisku_2 = digitalRead(Przycisk_2);
     StanPrzycisku_3 = digitalRead(Przycisk_3);
     SPWM = String(PWM); //czytaj od tyłu (prawej) Konwersja na string wartosc PWM i przypisanie do SPWM
-    Stme = String((millis() - time1) / 1000);
-    StmeB = String((millis() - time2) / 1000);
+    
+    if (abs(((millis() - czas_ref_trwania_programu) / 1000) - 60 * minuty) <= 10){
+      Stme = String("0") + String(minuty)+ String(":0") + String(abs(((millis() - czas_ref_trwania_programu) / 1000) - 60 * minuty));
+    }else if (minuty >= 10){
+      
+      Stme = String(minuty)+ String(":") + String(abs(((millis() - czas_ref_trwania_programu) / 1000) - 60 * minuty)); // dzielę przez 1000 bo wyświetlam sekundy.
+      
+    }else Stme = String(minuty)+ String(":") + String(abs(((millis() - czas_ref_trwania_programu) / 1000) - 60 * minuty));
+    
+    if (abs(((millis() - czas_ref_trwania_programu) / 1000) - 60 * minuty) >= 60){minuty++;}
     
     //Wykonuj co okreslony czas
-    if ((millis()-time2) >= BreakTime) {
+    if ((millis()-czas_ref_trwania_kierunku_obrotu) >= BreakTime) {
       analogWrite(Enable_B, PWM); //uruchom silnik.
       //inwersja zmiennych
       K1 = !K1;
@@ -210,22 +224,22 @@ void Program(String funkcja, unsigned long BreakTime = 0, unsigned long WTime = 
       //faktyczna inwersja na wyjściach
       digitalWrite(Wejscie_Sterownika_3, K1);
       digitalWrite(Wejscie_Sterownika_4, K2);
-      time2 = millis();
+      czas_ref_trwania_kierunku_obrotu = millis();
     }
     //kontrola szybkości
     if (!StanPrzycisku_3 && PWM != 0)   { PWM--; delay(10); }
     if (!StanPrzycisku_1 && PWM <= 254) { PWM++; delay(10); }
     
     if (!StanPrzycisku_2) {break;}
-    delay(100); //bez opoznienia nie ma wyswietlania bo tekst nawet nie zdazy sie pojawic na ekranie.
+    delay(100);
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print(funkcja +" "+ Stme + "s" + StmeB);
+    lcd.print(funkcja + Stme);
     lcd.leftToRight();
     lcd.setCursor(0,1); //drugi wiersz, pierwsza kolumna.
     lcd.print("<-  Przerwij  +>");
     
-  }analogWrite(Enable_B,PWM=0); //zerujemy PWM dla bezpieczenstwa.
+  }analogWrite(Enable_B,PWM=0); //zerujemy PWM dla bezpieczeństwa.
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Zakonczono prog.");
@@ -233,6 +247,7 @@ void Program(String funkcja, unsigned long BreakTime = 0, unsigned long WTime = 
   lcd.print(funkcja);
   delay(2000);
 }
+//definicje programów, wcześniej były tylko deklaracje.
 void Wywolywanie(){
     Program("Wywolywanie");
 }
@@ -248,9 +263,10 @@ void Plukanie(){
 void Uzytkownika(){
     Program("Uzytkownika");
 }
+//argumenty podawać w kolejności: Czas po którym zostanie zmieniony kierunek obrotów, czas trwania programu.
 void Predefined_D74(){
-    Program("Wywolywanie", 5000, 130000);
+    Program("Wywolywanie", 5000, 13000);
     Program("Przerywanie", 5000, 600);
     Program("Utrwalanie", 5000, 7000);
 }
-/********************************************************************/
+/********************************************************************************************************************************************/
